2a6a3e079c56b284ceb7002950d393c32679eb7d
Paulo  Diniz <paulo.rc.diniz@gmail.com>
2017-09-07 13:59:32 +0200

    testing not checking origin

--- config/prod.exs
+++ config/prod.exs
@@ -14,6 +14,7 @@ use Mix.Config
 config :flappy_server, FlappyServer.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
   url: [scheme: "https", host: "limitless-tor-81039.herokuapp.com", port: 443],
+  check_origin: false,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
